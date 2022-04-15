233fe993c42167c1c50e27c6ea8ea526db90fb6d
aninha <103117211+branchesana@users.noreply.github.com>
2022-04-15 18:20:33 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Hey</title>
+</head>
+<body>
+    <div> 
+        <h2> Playlist </h2>
+        <li> 5sos </li>
+        <li> yungblub </li>
+        <li> mgk </li>
+    </div>
+</body>
+</html>
